8d03a419d26759487911b61c50c80c2550813fee
User <tgbzdgn93@gmail.com>
2022-07-08 12:28:12 +0300

    merhaba 3

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -4,5 +4,7 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("merhaba 2");
+
+        System.out.println("merhaba 3");
     }
 }
